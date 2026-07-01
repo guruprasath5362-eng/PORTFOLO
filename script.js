2b56a3533cc08529368e00c65826cb5d3722239e
unknown <guruprasath5362@gmail.com>
2026-07-01 13:30:04 +0530

    new updated file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function openPopup() {
-    document.getElementById("popup").style.display = "block";
+    document.getElementById("popup").style.display = "flex";
 }
 
 function closePopup() {
@@ -7,20 +7,14 @@ function closePopup() {
 }
 
 function validateForm() {
-
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
-    let phone = document.getElementById("phone").value;
-    let message = document.getElementById("message").value;
-    let error = document.getElementById("error");
 
-    if(name=="" || email=="" || phone=="" || message==""){
-        error.style.display="block";
-        error.innerHTML="One or more fields have an error. Please check and try again.";
+    if (name === "" || email === "") {
+        document.getElementById("error").innerText = "Please fill all fields";
         return false;
     }
 
-    alert("Message Submitted Successfully!");
-    closePopup();
-    return false;
+    alert("Message sent successfully!");
+    return true;
 }
